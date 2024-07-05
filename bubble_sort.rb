96b27cc235cb04d3e15bdb0707b69ad65dd5c0ab
--global <107618227+chidi-locusnine@users.noreply.github.com>
2024-07-05 05:18:05 +0100

    --feat: implement bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(items)
+  isSorted = false;
+  until isSorted do
+    didNotSwap = true
+    items.each_index do |index|
+      break if (index == items.length - 1)
+
+      nextItem, currentItem = items[index + 1], items[index]
+
+      if (nextItem < currentItem) # do a swap
+        items[index], items[index + 1] = nextItem, currentItem 
+        didNotSwap = false
+      end
+    end
+
+    isSorted = didNotSwap 
+  end 
+
+  items
+end
+
+puts bubble_sort([4,3,78,2,0,2])
