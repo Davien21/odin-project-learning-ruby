e54e5b23c93d3af246f165af0151b08777377ec5
--global <107618227+chidi-locusnine@users.noreply.github.com>
2024-07-08 00:51:41 +0100

    --chore: lint with rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 A_ASCII = 'a'.ord
 Z_ASCII = 'z'.ord
 TOTAL_NO_OF_CHARS = 26 # From A - Z is 26 characters
@@ -10,27 +12,23 @@ end
 
 def caesar_cipher(word, shift_factor)
   capital_indexes = get_capital_indexes(word)
-  
+
   cypher = word.split('').map do |char|
     ascii_value = char.downcase.ord
 
-    if (ascii_value < A_ASCII || ascii_value > Z_ASCII)
-      next char
-    end
+    next char if ascii_value < A_ASCII || ascii_value > Z_ASCII
 
     ascii_value += shift_factor
-    
-    if ascii_value > Z_ASCII 
-      ascii_value -= TOTAL_NO_OF_CHARS
-    end
+
+    ascii_value -= TOTAL_NO_OF_CHARS if ascii_value > Z_ASCII
 
     ascii_value.chr
   end
 
   capital_indexes.each { |index| cypher[index].upcase! }
 
-  cypher.join()
+  cypher.join
 end
 
 # Example:
-p caesar_cipher("What a string!", 5)
+p caesar_cipher('What a string!', 5)
